fd2f305b317ccedd58ef7030f94b33b8ec7ebb4c
Gouthami Gopala Reddy <108340229+GouthamiGReddy@users.noreply.github.com>
2026-05-22 22:59:33 +0530

    Delete busproject directory

--- busproject/busapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .models import User, BusSlot, Booking
-
-admin.site.register(User, UserAdmin)
-admin.site.register(BusSlot)
-admin.site.register(Booking)
